33bb48aac70e2f78cd040b739e9f65a3d5b18a86
Peter Dønvang <peterdonvang@protonmail.com>
2024-07-29 21:39:30 +0200

    started refactor

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,13 @@
-import { Engine } from "excalibur";
-import { Player } from "./player";
-import { loader } from "./resources";
-import { Roll } from "./roll";
-import * as ex from 'excalibur';
-import { Sheet } from "./sheet";
+// class Roll {
+//   isGrabbed: boolean = false;
+//   sheetsLeft: number = 100;
+//   clickY: number = 0;
+//   width: number = 100;
+// }
 
+const roll = document.getElementById('roll')
 
-class Game extends Engine {
-    constructor() {
-      super({
-        width: 1024, 
-        height: 800,
-        physics: {
-          solver: ex.SolverStrategy.Realistic
-        }
-      });
-    }
+roll.style.backgroundColor = 'white'
 
-    setPhysics = () => {
-      // setting physics and gravity
-      game.currentScene.physics.config.solver = ex.SolverStrategy.Realistic;
-      game.currentScene.physics.config.gravity = ex.vec(0, 300)
-    }
-
-    initialize() {
-
-
-      const player = new Player();
-      this.add(player);
-      
-      const roll = new Roll();
-      roll.pos = ex.vec(game.halfCanvasWidth, game.halfCanvasHeight);
-      
-      this.add(roll)
-      
-      console.log(this.input.pointers.primary.lastPagePos)
-      this.setPhysics()
-      this.start(loader);
-    }
-  }
-  
-  export const game = new Game();
-  game.initialize();
+roll?.style.height = '100px';
+roll?.style.width = '100px';
